401146f53690bc997c7720c799108fb215f1716c
Cindy1229 <leticiashi3@gmail.com>
2020-08-04 18:55:59 -0400

    weather app version 2.0 init

---
+++ src/app.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+//Export functions
+const geoCode=require('./utils/geocode.js');
+const forecast=require('./utils/forecast.js');
+
+// Paths for express config
+const path = require('path');
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+//handlebars setup
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Setup static directory
+app.use(express.static(publicDirectoryPath));
+
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Cindy'
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Cindy'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'Send me an email to conatct me!!',
+        title: 'Help Page',
+        name: 'Cindy'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            error: 'please enter address'
+        })
+    }
+
+    geoCode(req.query.address, (error, {latitude, longitude, location}={})=>{
+        if (error) {
+            return res.send({
+               error
+            });
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({
+                    error
+                });
+            }
+            res.send({
+                location,
+                forecastData
+            });
+        })
+    })
+    
+})
+
+app.get('/help/*', (req, res)=>{
+    res.render('404', {
+        errorText: 'Help not found.',
+        title: 'OOPS!',
+        name: 'Cindy'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        errorText: 'Page not found.',
+        title: 'OOPS!',
+        name: 'Cindy'
+    })
+})
+
+//satrt up the server
+app.listen(3000, () => {
+    console.log('server is up on port 3000');
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request');
+const forecast=(latitude, longitude, callback)=>{
+    const url=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&%20exclude={part}&appid=672a1acbfbd8f8852bd51abc05329ca8&units=metric`;
+
+    request({url, json:true}, (error,{body})=>{
+        if(error){
+            callback('Unable to connect to forecast services.', undefined);
+        }
+        else if(body.cod){
+            callback('Unable to find the coordinates.', undefined);
+
+        }
+        else {
+            callback(undefined, body.current.weather);
+        }
+    })
+};
+
+module.exports=forecast;
